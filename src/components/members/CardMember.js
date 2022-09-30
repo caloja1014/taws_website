eb079f6dc6b4cf863307bc7c85efe716688451eb
caloja1014 <caloja@espol.edu.ec>
2022-09-29 20:10:04 -0500

    responsive design members

--- src/components/members/CardMember.js
+++ src/components/members/CardMember.js
@@ -8,16 +8,18 @@ const CardMember = ({ name, image, title, linkLinkedin, linkGithub }) => {
       <div className='taws-card-member-image'>
         <img className='taws-card-member-image-inner' src={image} alt={name} />
       </div>
-      <div className='taws-card-member-nam taws-firacode-bold'>
-        {name}
-      </div>
-      <div className='taws-card-member-title taws-firacode-bold'>
-        {title}
-      </div>
+      <div>
+        <div className='taws-card-member-nam taws-firacode-bold'>
+          {name}
+        </div>
+        <div className='taws-card-member-title taws-firacode-bold'>
+          {title}
+        </div>
         <div className='taws-icon-directive'>
           <img id='taws-card-github' src={require('../assets/github_directive.png')} alt='' />
           <img id='taws-card-linkedin' src={require('../assets/linkedin_directive.png')} alt='' />
         </div>
+      </div>
     </div>
   );
 };
--- src/components/members/index.js
+++ src/components/members/index.js
@@ -5,23 +5,30 @@ import CardMember from './CardMember';
 const Members = () => {
     return (
         <div className='taws-member'>
-            <div>
-                <CardMember 
-                name='Joangie Marquéz' 
-                image={require("../assets/jojo.png")} 
-                title='Vicepresidente académico' />
+            <div className='taws-member-title'>
+                <span className='taws-title-directive taws-firacode-bold'>
+                    Directiva
+                </span>
             </div>
-            <div>
-                <CardMember 
-                name='Daniela Landeta' 
-                image={require("../assets/daniela.png")} 
-                title='Presidente' />
-            </div>
-            <div>
-                <CardMember 
-                name='Joseph Ávila' 
-                image={require("../assets/joseph.png")} 
-                title='Vicepresidente administrativo' />
+            <div className='taws-directive'>
+                <div>
+                    <CardMember
+                        name='Joangie Marquéz'
+                        image={require("../assets/jojo.png")}
+                        title='Vicepresidente académico' />
+                </div>
+                <div>
+                    <CardMember
+                        name='Daniela Landeta'
+                        image={require("../assets/daniela.png")}
+                        title='Presidente' />
+                </div>
+                <div>
+                    <CardMember
+                        name='Joseph Ávila'
+                        image={require("../assets/joseph.png")}
+                        title='Vicepresidente administrativo' />
+                </div>
             </div>
         </div>
     );
